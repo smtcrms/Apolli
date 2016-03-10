86730ebe9b670ba4694a2e6e731cc59e7d36d4c2
Yiming <liuym@Ctrip.com>
2016-03-10 21:59:51 +0800

    Add Application code for submodule

---
+++ apollo-portal/src/main/java/com/ctrip/apollo/portal/PortalApplication.java
@@ -0,0 +1,11 @@
+package com.ctrip.apollo.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(PortalApplication.class, args);
+  }
+}
