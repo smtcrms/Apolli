53ed634283844ab4c1bf2e28f1cb5d95ea51eeee
Yiming Liu <liuyiming.vip@gmail.com>
2016-03-22 13:38:17 +0800

    Merge MetaServer into ConfigServer

--- apollo-configserver/src/main/java/com/ctrip/apollo/ServerApplication.java
+++ apollo-configserver/src/main/java/com/ctrip/apollo/ServerApplication.java
@@ -1,17 +1,19 @@
 package com.ctrip.apollo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Spring boot application entry point
  * @author Jason Song(song_s@ctrip.com)
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
+@EnableEurekaClient
 public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+      new SpringApplicationBuilder(ServerApplication.class).web(true).run(args);
     }
 }
