48c2474b6a8b7e058134f7d92daf65f97991f874
Yiming Liu <liuyiming.vip@gmail.com>
2016-04-11 23:50:38 +0800

    Add CRUD for Cluster and Namespace Entity

--- apollo-biz/src/main/java/com/ctrip/apollo/biz/service/ClusterService.java
+++ apollo-biz/src/main/java/com/ctrip/apollo/biz/service/ClusterService.java
@@ -3,8 +3,10 @@ package com.ctrip.apollo.biz.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ctrip.apollo.biz.entity.App;
 import com.ctrip.apollo.biz.entity.Cluster;
 import com.ctrip.apollo.biz.repository.ClusterRepository;
+import com.ctrip.apollo.biz.utils.BeanUtils;
 
 @Service
 public class ClusterService {
@@ -14,5 +16,20 @@ public class ClusterService {
 
   public Cluster findOne(String appId, String name) {
     return clusterRepository.findByAppIdAndName(appId, name);
+  }
+
+  public Cluster save(Cluster entity) {
+    return clusterRepository.save(entity);
+  }
+
+  public void delete(long id) {
+    clusterRepository.delete(id);
+  }
+
+  public Cluster update(Cluster cluster) {
+    Cluster managedCluster =
+        clusterRepository.findByAppIdAndName(cluster.getAppId(), cluster.getName());
+    BeanUtils.copyEntityProperties(cluster, managedCluster);
+    return clusterRepository.save(managedCluster);
   }
 }
