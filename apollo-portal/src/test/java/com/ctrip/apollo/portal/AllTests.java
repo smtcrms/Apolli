bef96d6f90f85cf4719fb6cd5993ab2b086ed227
lepdou <ledou.zhang@dianping.com>
2016-04-15 14:07:06 +0800

    delete old test

--- apollo-portal/src/test/java/com/ctrip/apollo/portal/AllTests.java
+++ apollo-portal/src/test/java/com/ctrip/apollo/portal/AllTests.java
@@ -4,11 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.ctrip.apollo.portal.service.ConfigServiceTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
-  //ConfigServiceTest.class
   })
 public class AllTests {
 
