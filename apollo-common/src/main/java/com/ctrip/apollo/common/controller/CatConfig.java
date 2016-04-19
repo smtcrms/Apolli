464da4de13a605b5260a47dcd849c3cc43b58776
Yiming Liu <liuyiming.vip@gmail.com>
2016-04-19 11:35:16 +0800

    Update cat bean definition

--- apollo-common/src/main/java/com/ctrip/apollo/common/controller/CatConfig.java
+++ apollo-common/src/main/java/com/ctrip/apollo/common/controller/CatConfig.java
@@ -1,5 +1,9 @@
 package com.ctrip.apollo.common.controller;
 
+import javax.servlet.DispatcherType;
+
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +14,20 @@ import com.dianping.cat.servlet.CatListener;
 public class CatConfig {
 
   @Bean
-  public CatFilter catFilter() {
-    return new CatFilter();
+  public FilterRegistrationBean catFilter() {
+    FilterRegistrationBean bean = new FilterRegistrationBean();
+    bean.setFilter(new CatFilter());
+    bean.setName("cat-filter");
+    bean.addUrlPatterns("/*");
+    bean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.FORWARD);
+    return bean;
   }
 
   @Bean
-  public CatListener catListener() {
-    return new CatListener();
+  public ServletListenerRegistrationBean<CatListener> catListener() {
+    ServletListenerRegistrationBean<CatListener> bean =
+        new ServletListenerRegistrationBean<CatListener>(new CatListener());
+    bean.setName("cat-listener");
+    return bean;
   }
 }
