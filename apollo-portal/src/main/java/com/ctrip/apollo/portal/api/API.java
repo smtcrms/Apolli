4054304f61d5e0f1254f44cb058b11ff344feda4
Yiming Liu <liuyiming.vip@gmail.com>
2016-04-19 10:03:11 +0800

    Inject authed RestTemplate into Portal

--- apollo-portal/src/main/java/com/ctrip/apollo/portal/api/API.java
+++ apollo-portal/src/main/java/com/ctrip/apollo/portal/api/API.java
@@ -1,11 +1,14 @@
 package com.ctrip.apollo.portal.api;
 
-import com.ctrip.apollo.core.enums.Env;
-import com.ctrip.apollo.core.exception.ServiceException;
-import com.ctrip.apollo.portal.service.ServiceLocator;
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
+
+import com.ctrip.apollo.common.auth.RestTemplateFactory;
+import com.ctrip.apollo.core.enums.Env;
+import com.ctrip.apollo.core.exception.ServiceException;
+import com.ctrip.apollo.portal.service.ServiceLocator;
 
 public class API {
 
@@ -13,7 +16,14 @@ public class API {
   protected ServiceLocator serviceLocator;
 
   @Autowired
+  private RestTemplateFactory restTemplateFactory;
+
   protected RestTemplate restTemplate;
+
+  @PostConstruct
+  private void postConstruct() {
+    restTemplate = restTemplateFactory.getObject();
+  }
 
   public String getAdminServiceHost(Env env) {
     // 本地测试用
@@ -25,5 +35,5 @@ public class API {
     }
     return "";
   }
-  
+
 }
