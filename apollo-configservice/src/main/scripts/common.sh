d6dd288cfb9c4d51a71e37fec158811e641e7a64
Yiming Liu <liuyiming.vip@gmail.com>
2016-04-22 16:06:13 +0800

    Fix script typo

--- apollo-configservice/src/main/scripts/common.sh
+++ apollo-configservice/src/main/scripts/common.sh
@@ -9,7 +9,7 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> $SYSOUT_LOG >> SYSOUT_LOG &
+            nohup java -jar $PATH_TO_JAR /tmp 2>> $SYSOUT_LOG >> $SYSOUT_LOG &
                         echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
@@ -35,7 +35,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR /tmp 2>> SYSOUT_LOG >> SYSOUT_LOG &
+            nohup java -jar $PATH_TO_JAR /tmp 2>> $SYSOUT_LOG >> $SYSOUT_LOG &
                         echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
