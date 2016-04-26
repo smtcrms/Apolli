35db3822928ffd5cfc9f560b7864f3da72dc2706
lepdou <ledou.zhang@dianping.com>
2016-04-26 11:09:31 +0800

    fix service locator bug

--- apollo-portal/src/main/java/com/ctrip/apollo/portal/service/ServiceLocator.java
+++ apollo-portal/src/main/java/com/ctrip/apollo/portal/service/ServiceLocator.java
@@ -41,18 +41,18 @@ public class ServiceLocator {
 
   public ServiceDTO getAdminService(Env env) throws ServiceException {
     List<ServiceDTO> services = getServices(env, "admin");
-    if (services.size() == 0) {
+    if (services == null || services.size() == 0) {
       throw new ServiceException("No available admin service");
     }
-    return services.get(adminCallCounts.getAndIncrement() % services.size());
+    return services.get(Math.abs(adminCallCounts.getAndIncrement()) % services.size());
   }
 
   public ServiceDTO getConfigService(Env env) throws ServiceException {
     List<ServiceDTO> services = getServices(env, "config");
-    if (services.size() == 0) {
+    if (services == null || services.size() == 0) {
       throw new ServiceException("No available config service");
     }
-    return services.get(configCallCounts.getAndIncrement() % services.size());
+    return services.get(Math.abs(configCallCounts.getAndIncrement()) % services.size());
   }
 
   private List<ServiceDTO> getServices(Env env, String serviceUrl) {
