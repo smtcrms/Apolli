97ad363b90739c8c4f3e7dbe389b187fc5a64b40
Jason Song <nobodyiam@gmail.com>
2016-05-03 18:40:33 +0800

    update portal script and add jdk1.8 identifier

--- apollo-portal/src/main/scripts/shutdown.sh
+++ apollo-portal/src/main/scripts/shutdown.sh
@@ -2,6 +2,12 @@
 SERVICE_NAME=apollo-portal
 
 cd `dirname $0`/..
+
+if [[ ! -f $SERVICE_NAME".jar" && -d current ]]; then
+    cd current
+fi
+
 if [[ -f $SERVICE_NAME".jar" ]]; then
+  chmod a+x $SERVICE_NAME".jar"
   ./$SERVICE_NAME".jar" stop
 fi
--- apollo-portal/src/main/scripts/startup.sh
+++ apollo-portal/src/main/scripts/startup.sh
@@ -5,11 +5,16 @@ PATH_TO_JAR=$SERVICE_NAME"-"$VERSION".jar"
 
 cd `dirname $0`/..
 
+if [[ ! -f PATH_TO_JAR && -d current ]]; then
+    cd current
+fi
+
 if [[ -f $SERVICE_NAME".jar" ]]; then
   rm -rf $SERVICE_NAME".jar"
 fi
 
 ln $PATH_TO_JAR $SERVICE_NAME".jar"
+chmod a+x $SERVICE_NAME".jar"
 ./$SERVICE_NAME".jar" start
 
 exit 0;
