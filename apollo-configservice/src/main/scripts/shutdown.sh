6022759b1f975bb9b974e223d38964699fd8a658
Jason Song <nobodyiam@gmail.com>
2016-05-03 17:38:06 +0800

    compatibility for cd portal envionment

--- apollo-configservice/src/main/scripts/shutdown.sh
+++ apollo-configservice/src/main/scripts/shutdown.sh
@@ -2,6 +2,11 @@
 SERVICE_NAME=apollo-configservice
 
 cd `dirname $0`/..
+
+if [[ ! -f $SERVICE_NAME".jar" && -d current ]]; then
+    cd current
+fi
+
 if [[ -f $SERVICE_NAME".jar" ]]; then
   ./$SERVICE_NAME".jar" stop
 fi
