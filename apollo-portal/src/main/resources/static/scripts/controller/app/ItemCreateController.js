58d59c1f1a3b72f3424a41be01d7f017d64571b2
lepdou <ledou.zhang@dianping.com>
2016-05-19 17:03:43 +0800

    .

---
+++ apollo-portal/src/main/resources/static/scripts/controller/app/ItemCreateController.js
@@ -0,0 +1,14 @@
+application_module.controller("ItemCreateController",
+                              ['$rootScope', '$scope', '$location', 'toastr', 'AppService', 'AppUtil',
+                               function ($rootScope, $scope, $location, toastr, AppService, AppUtil) {
+
+                                   $scope.create = function () {
+                                       console.log("xx");
+
+                                   };
+
+                                   $scope.key = 'xx';
+
+                                  
+                               }]);
+
