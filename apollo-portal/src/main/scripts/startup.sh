38b88cfb7d5e550e165509204a094ad3b8cfb3eb
Jason Song <nobodyiam@gmail.com>
2016-06-03 11:35:29 +0800

    Refactor startup logic to wait until server is fully startup

--- apollo-portal/src/main/scripts/startup.sh
+++ apollo-portal/src/main/scripts/startup.sh
@@ -5,39 +5,7 @@ PATH_TO_JAR=$SERVICE_NAME".jar"
 export JAVA_OPTS="-server -Xms8192m -Xmx8192m -Xss256k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=384m -XX:NewSize=3072m -XX:MaxNewSize=3072m -XX:SurvivorRatio=22 -XX:+UseParNewGC -XX:ParallelGCThreads=4 -XX:MaxTenuringThreshold=9 -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+UseCMSCompactAtFullCollection -XX:+CMSParallelRemarkEnabled -XX:CMSFullGCsBeforeCompaction=9 -XX:CMSInitiatingOccupancyFraction=60 -XX:+CMSClassUnloadingEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:-ReduceInitialCardMarks -XX:+CMSPermGenSweepingEnabled -XX:CMSInitiatingPermOccupancyFraction=70 -XX:+ExplicitGCInvokesConcurrent -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationConcurrentTime -XX:+PrintHeapAtGC -XX:+HeapDumpOnOutOfMemoryError -XX:-OmitStackTraceInFastThrow -Duser.timezone=Asia/Shanghai -Dclient.encoding.override=UTF-8 -Dfile.encoding=UTF-8"
 export JAVA_OPTS="$JAVA_OPTS -Xloggc:/opt/logs/100003173/heap_trace.txt -XX:HeapDumpPath=/opt/logs/100003173/HeapDumpOnOutOfMemoryError/"
 
-if [[ -z "$JAVA_HOME" && -d /usr/java/latest/ ]]; then
-    export JAVA_HOME=/usr/java/latest/
-fi
+SERVER_PORT=8080
+SERVER_URL="http://localhost:$SERVER_PORT"
 
-cd `dirname $0`/..
-
-for i in `ls $SERVICE_NAME-*.jar 2>/dev/null`
-do
-    if [[ ! $i == *"-sources.jar" ]]
-    then
-        PATH_TO_JAR=$i
-        break
-    fi
-done
-
-if [[ ! -f PATH_TO_JAR && -d current ]]; then
-    cd current
-    for i in `ls $SERVICE_NAME-*.jar 2>/dev/null`
-    do
-        if [[ ! $i == *"-sources.jar" ]]
-        then
-            PATH_TO_JAR=$i
-            break
-        fi
-    done
-fi
-
-if [[ -f $SERVICE_NAME".jar" ]]; then
-  rm -rf $SERVICE_NAME".jar"
-fi
-
-ln $PATH_TO_JAR $SERVICE_NAME".jar"
-chmod a+x $SERVICE_NAME".jar"
-./$SERVICE_NAME".jar" start
-
-exit 0;
+source ./common_startup.sh
