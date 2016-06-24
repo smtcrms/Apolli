2ca490a1d0d1cf410509b56254f7a1b8e3b303a3
lepdou <lepdou@126.com>
2016-06-24 15:34:36 +0800

    update

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/ClusterService.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/ClusterService.java
@@ -3,7 +3,6 @@ package com.ctrip.framework.apollo.portal.service;
 import com.ctrip.framework.apollo.core.enums.Env;
 import com.ctrip.framework.apollo.core.dto.ClusterDTO;
 import com.ctrip.framework.apollo.portal.api.AdminServiceAPI;
-import com.ctrip.framework.apollo.portal.auth.UserInfoHolder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +13,6 @@ import java.util.List;
 public class ClusterService {
 
   @Autowired
-  private UserInfoHolder userInfoHolder;
-  @Autowired
   private AdminServiceAPI.ClusterAPI clusterAPI;
 
   public List<ClusterDTO> findClusters(Env env, String appId){
@@ -23,9 +20,6 @@ public class ClusterService {
   }
 
   public ClusterDTO createCluster(Env env, ClusterDTO cluster){
-    String operator = userInfoHolder.getUser().getUserId();
-    cluster.setDataChangeLastModifiedBy(operator);
-    cluster.setDataChangeCreatedBy(operator);
     return clusterAPI.createOrUpdate(env, cluster);
   }
 
