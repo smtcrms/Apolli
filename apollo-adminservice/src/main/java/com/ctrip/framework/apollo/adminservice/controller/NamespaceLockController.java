75199e3baa4f5d1d5cae18a74f0734762b6278f2
lepdou <lepdou@126.com>
2016-06-30 11:56:45 +0800

    fix when namespace.lock.switch off

--- apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/controller/NamespaceLockController.java
+++ apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/controller/NamespaceLockController.java
@@ -4,6 +4,7 @@ import com.ctrip.framework.apollo.biz.entity.Namespace;
 import com.ctrip.framework.apollo.biz.entity.NamespaceLock;
 import com.ctrip.framework.apollo.biz.service.NamespaceLockService;
 import com.ctrip.framework.apollo.biz.service.NamespaceService;
+import com.ctrip.framework.apollo.biz.utils.ApolloSwitcher;
 import com.ctrip.framework.apollo.common.utils.BeanUtils;
 import com.ctrip.framework.apollo.core.dto.NamespaceLockDTO;
 import com.ctrip.framework.apollo.core.exception.BadRequestException;
@@ -17,23 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NamespaceLockController {
 
+
   @Autowired
   private NamespaceLockService namespaceLockService;
   @Autowired
   private NamespaceService namespaceService;
-
+  @Autowired
+  private ApolloSwitcher apolloSwitcher;
 
   @RequestMapping("/apps/{appId}/clusters/{clusterName}/namespaces/{namespaceName}/lock")
   public NamespaceLockDTO getNamespaceLockOwner(@PathVariable String appId, @PathVariable String clusterName,
-                                                @PathVariable String namespaceName){
+                                                @PathVariable String namespaceName) {
     Namespace namespace = namespaceService.findOne(appId, clusterName, namespaceName);
-    if (namespace == null){
+    if (namespace == null) {
       throw new BadRequestException("namespace not exist.");
+    }
+
+    if (apolloSwitcher.isNamespaceLockSwitchOff()) {
+      throw new NotFoundException(namespaceName + " is not locked");
     }
 
     NamespaceLock lock = namespaceLockService.findLock(namespace.getId());
 
-    if (lock == null){
+    if (lock == null) {
       throw new NotFoundException(namespaceName + " is not locked");
     }
 
