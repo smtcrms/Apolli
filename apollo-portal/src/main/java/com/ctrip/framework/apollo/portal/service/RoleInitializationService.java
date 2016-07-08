81dc3b9ce4b435ce3607bab376758cf2a168ba41
lepdou <lepdou@126.com>
2016-07-08 16:49:06 +0800

    update

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/RoleInitializationService.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/RoleInitializationService.java
@@ -37,7 +37,7 @@ public class RoleInitializationService {
       return;
     }
     String operaterUserId = userInfoHolder.getUser().getUserId();
-    //createItem app permissions
+    //create app permissions
     createAppMasterRole(appId);
 
     //assign master role to user
@@ -75,7 +75,7 @@ public class RoleInitializationService {
         appPermissionIds =
         FluentIterable.from(createdAppPermissions).transform(permission -> permission.getId()).toSet();
 
-    //createItem app master role
+    //create app master role
     Role appMasterRole = createRole(RoleUtils.buildAppMasterRoleName(appId));
 
     rolePermissionService.createRoleWithPermissions(appMasterRole, appPermissionIds);
