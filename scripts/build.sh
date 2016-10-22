78e058aaf4d4a4de37fc1187254fc39274b48772
nobodyiam <nobodyiam@gmail.com>
2016-10-22 15:15:32 +0800

    adapt for github deployment

---
+++ scripts/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# apollo config db info
+apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_username=root
+apollo_config_db_password=
+
+# apollo portal db info
+apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_username=root
+apollo_portal_db_password=
+
+# meta server url
+dev_meta=http://localhost:8080
+fat_meta=http://localhost:8080
+uat_meta=http://localhost:8080
+pro_meta=http://localhost:8080
+
+META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
+
+# =============== Please do not modify the following content =============== #
+cd ..
+
+# package config-service and admin-service
+echo "==== starting to build config-service and admin-service ===="
+
+mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Pgithub -Dapollo_profile=dev -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password
+
+echo "==== building config-service and admin-service finished ===="
+
+echo "==== starting to build portal ===="
+
+mvn clean package -DskipTests -pl apollo-portal -am -Pgithub -Dapollo_profile=dev -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password $META_SERVERS_OPTS
+
+echo "==== building portal finished ===="
+
+echo "==== starting to build client ===="
+
+mvn clean install -DskipTests -pl apollo-client -am -Pgithub $META_SERVERS_OPTS
+
+echo "==== building portal finished ===="
+
+
+
