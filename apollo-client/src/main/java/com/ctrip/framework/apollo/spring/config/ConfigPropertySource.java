c567bdd220fa68181d2370d3507948a4231675e1
Jason Song <nobodyiam@gmail.com>
2017-03-06 14:17:08 +0800

    fix for Spring 4.1.2 that composite property source should extend EnumerablePropertySource

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/ConfigPropertySource.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/ConfigPropertySource.java
@@ -1,17 +1,29 @@
 package com.ctrip.framework.apollo.spring.config;
 
-import com.ctrip.framework.apollo.Config;
+import java.util.Set;
+import org.springframework.core.env.EnumerablePropertySource;
 
-import org.springframework.core.env.PropertySource;
+import com.ctrip.framework.apollo.Config;
 
 /**
  * Property source wrapper for Config
  *
  * @author Jason Song(song_s@ctrip.com)
  */
-public class ConfigPropertySource extends PropertySource<Config> {
+public class ConfigPropertySource extends EnumerablePropertySource<Config> {
+  private static final String[] EMPTY_ARRAY = new String[0];
+
   public ConfigPropertySource(String name, Config source) {
     super(name, source);
+  }
+
+  @Override
+  public String[] getPropertyNames() {
+    Set<String> propertyNames = this.source.getPropertyNames();
+    if (propertyNames.isEmpty()) {
+      return EMPTY_ARRAY;
+    }
+    return propertyNames.toArray(new String[propertyNames.size()]);
   }
 
   @Override
