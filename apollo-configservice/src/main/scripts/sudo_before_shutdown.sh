4221dc663fd6a2ec402c8f7a84f66c6c5165a52f
nobodyiam <nobodyiam@gmail.com>
2017-04-27 22:07:59 +0800

    remove sudo before shutdown as it is not needed anymore

--- apollo-configservice/src/main/scripts/sudo_before_shutdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if grep -Fq "LimitNOFILE" /usr/lib/systemd/system/ctripapp\@100003171.service; then
-       echo "already set LimitNOFILE";
-else
-       sed -i '/\[Service\]/a\LimitNOFILE=65536' /usr/lib/systemd/system/ctripapp\@100003171.service;
-       systemctl daemon-reload
-fi
