a80727dc0c4004ff6f4059dc30db741efe34e9f3
lepdou <lepdou@126.com>
2017-06-23 14:52:19 +0800

    EnvUtils add transform tools env

--- apollo-core/src/main/java/com/ctrip/framework/apollo/core/enums/EnvUtils.java
+++ apollo-core/src/main/java/com/ctrip/framework/apollo/core/enums/EnvUtils.java
@@ -23,6 +23,8 @@ public final class EnvUtils {
         return Env.DEV;
       case "LOCAL":
         return Env.LOCAL;
+      case "TOOLS":
+        return Env.TOOLS;
       default:
         return null;
     }
