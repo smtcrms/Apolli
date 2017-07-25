d34c91fc2491d64a59a3bb9164bbae10bb94c448
nobodyiam <nobodyiam@gmail.com>
2017-07-25 22:14:24 +0800

    add more refresh scope sample

--- apollo-demo/src/main/java/com/ctrip/framework/apollo/demo/spring/common/config/AppConfig.java
+++ apollo-demo/src/main/java/com/ctrip/framework/apollo/demo/spring/common/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.ctrip.framework.apollo.demo.spring.common.bean.NormalBean;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableApolloConfig(value = "application", order = 10)
 public class AppConfig {
-  @Bean
+  @Bean("normalBean")
+  @RefreshScope
   public NormalBean normalBean(@Value("${batch:100}") int batch) {
     NormalBean bean = new NormalBean();
     bean.setBatch(batch);
