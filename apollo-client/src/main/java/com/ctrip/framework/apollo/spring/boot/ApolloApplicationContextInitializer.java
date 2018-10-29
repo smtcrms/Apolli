ac2baee2cde66abb50cbf8695a5d6ea97a3b191a
Luan Louis <louemail@qq.com>
2018-10-29 19:48:05 +0800

    增加EnvironmentPostProcessor处理，将Apollo配置加载提到初始化日志系统之前

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloApplicationContextInitializer.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloApplicationContextInitializer.java
@@ -8,13 +8,16 @@ import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
 import com.ctrip.framework.apollo.spring.util.SpringInjector;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.List;
 
 /**
  * Initialize apollo system properties and inject the Apollo config in Spring Boot bootstrap phase
@@ -37,9 +40,23 @@ import org.springframework.core.env.ConfigurableEnvironment;
  *   # will inject 'application' and 'FX.apollo' namespaces in bootstrap phase
  *   apollo.bootstrap.namespaces = application,FX.apollo
  * </pre>
+ *
+ *
+ * If you want to load Apollo configurations even before Logging System Initialization Phase,
+ *  add
+ * <pre class="code">
+ *   # set apollo.bootstrap.eagerLoad.enabled
+ *   apollo.bootstrap.eagerLoad.enabled = true
+ * </pre>
+ *
+ *  This would be very helpful when  your logging configurations is set by Apollo.
+ *
+ *  for example, you have defined  logback-spring.xml in your project , and you want to inject some attributes into logback-spring.xml.
+ *
+ *
  */
 public class ApolloApplicationContextInitializer implements
-    ApplicationContextInitializer<ConfigurableApplicationContext> {
+    ApplicationContextInitializer<ConfigurableApplicationContext> , EnvironmentPostProcessor {
   private static final Logger logger = LoggerFactory.getLogger(ApolloApplicationContextInitializer.class);
   private static final Splitter NAMESPACE_SPLITTER = Splitter.on(",").omitEmptyStrings().trimResults();
   private static final String[] APOLLO_SYSTEM_PROPERTIES = {"app.id", ConfigConsts.APOLLO_CLUSTER_KEY,
@@ -60,6 +77,20 @@ public class ApolloApplicationContextInitializer implements
       return;
     }
     logger.debug("Apollo bootstrap config is enabled for context {}", context);
+
+    initialize(environment);
+  }
+
+
+  /**
+   *
+   *
+   * Initialize Apollo Configurations Just after environment is ready.
+   *
+   *
+   * @param environment
+   */
+  protected void initialize(ConfigurableEnvironment environment) {
 
     if (environment.getPropertySources().contains(PropertySourcesConstants.APOLLO_BOOTSTRAP_PROPERTY_SOURCE_NAME)) {
       //already initialized
@@ -101,5 +132,40 @@ public class ApolloApplicationContextInitializer implements
     }
 
     System.setProperty(propertyName, propertyValue);
+  }
+
+  /**
+   *
+   * In order to load  Apollo configurations even as early before Spring loading logging system phase,
+   *
+   *  This EnvironmentPostProcessor can be called Just After ConfigFileApplicationListener has succeeded.
+   *
+   *
+   *  The processing sequence would be like this:
+   *
+   *  Load Bootstrap properties and application properties -----> load Apollo configuration properties ----> Initialize Logging systems
+   *
+   *
+   *
+   * @param configurableEnvironment
+   * @param springApplication
+   */
+  @Override
+  public void postProcessEnvironment(ConfigurableEnvironment configurableEnvironment, SpringApplication springApplication) {
+
+    Boolean eagerLoadEnabled  = configurableEnvironment.getProperty(PropertySourcesConstants.APOLLO_BOOTSTRAP_EAGER_LOAD_ENABLED,Boolean.class,false);
+
+
+    //EnvironmentPostProcessor should not be triggered if you don't want Apollo Loading before Logging System Initialization
+    if(!eagerLoadEnabled){
+      return;
+    }
+
+    Boolean bootstrapEnabled = configurableEnvironment.getProperty(PropertySourcesConstants.APOLLO_BOOTSTRAP_ENABLED,Boolean.class,false);
+
+    if(bootstrapEnabled) {
+        initialize(configurableEnvironment);
+    }
+
   }
 }
